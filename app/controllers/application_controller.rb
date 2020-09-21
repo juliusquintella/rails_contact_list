334c2a3a40d155ac6b4e2933e40b02f040531e4a
Julius Quintella <julius.quintella@gmail.com>
2020-09-21 18:30:02 -0300

    teste

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   private
     def require_logged_in_user
       unless user_signed_in?
-        flash[:danger] = 'Área restrita. Por favor, realize o login.'
+        flash[:danger] = 'Área restrita. Por favor, realize o login!'
         redirect_to entrar_url
       end
     end
